b8fd6d15293ac64c06caefb95022ac1c9018732d
RedHotUnicorn <RedHotUnicorn@users.noreply.github.com>
2023-06-30 18:23:21 +0300

    work files with OBS

---
+++ OBS_to_DB_read_csv_urls.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+import PDButils as u
+
+
+
+df = pd.DataFrame()
+
+for root, dirs, files in os.walk(u.VAULT_CSV_PATH):
+    for file in files:
+        if file.endswith(".csv"):
+            # print(os.path.join(root, file))
+            tmp  = pd.read_csv(os.path.join(root, file)
+                                , usecols =['URL']
+                                , sep=';'
+                                )
+            tmp['file'] = file
+            
+            df = df.append(tmp, ignore_index = True)
+
+print(df)
