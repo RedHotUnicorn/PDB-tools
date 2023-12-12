f583beb309d3debbd0d17912ddf6176ec8757a9f
RedHotUnicorn <RedHotUnicorn@users.noreply.github.com>
2023-12-13 01:00:53 +0300

    try render_html

--- _test_trafilatura_codeblock.py
+++ _test_trafilatura_codeblock.py
@@ -1,5 +1,6 @@
 import trafilatura
 from bs4 import BeautifulSoup
+import requests
 
 
 url="https://microsoft.github.io/lida/"
@@ -8,6 +9,21 @@ url="https://microsoft.github.io/lida/"
 # url = "https://rxresu.me/"
 # url = "https://stackoverflow.com/a/69178995"
 url = 'https://nesslabs.com/best'  
+url = "https://microsoft.github.io/generative-ai-for-beginners/#/"
+url = 'https://www.reddit.com/r/Zettelkasten/s/tV7CFNg5rZ'
+
+from requests_html import HTMLSession
+
+session = HTMLSession()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+r = session.get(url,headers=headers,allow_redirects=True)
+
+r.html.render()
+
+
+f = open("out/_test_trafiltura_requests.html", "w",encoding='utf-8')
+f.write(r.html.html)
+f.close()
 
 downloaded = trafilatura.fetch_url(url)
 f = open("out/_test_trafiltura_downloaded.html", "w",encoding='utf-8')
@@ -15,7 +31,7 @@ f.write(downloaded)
 f.close()
 
 
-downloaded_bs = BeautifulSoup(downloaded, features="html.parser")
+downloaded_bs = BeautifulSoup(r.html.html, features="html.parser")
 [tag.attrs.clear() for tag in downloaded_bs.find_all(['pre','code',"li"])]
 
 
