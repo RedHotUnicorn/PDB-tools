f2ffae1118169a35e7ddd95e1d130a9fd8d8a6fa
RedHotUnicorn <RedHotUnicorn@users.noreply.github.com>
2023-12-16 14:44:18 +0300

    subs + tsv agregate merge

--- _test_yt_dlp.py
+++ _test_yt_dlp.py
@@ -1,44 +1,79 @@
 import yt_dlp
 import _utils as u
 import os
+from    pathlib import Path
+from _test_tsv_agregate import tsv_to_md
 
 url = "https://www.youtube.com/watch?v=Z8yL3zkudZU"
 url = 'https://www.youtube.com/watch?v=P1u3UZQmaRo' 
-url = 'https://www.youtube.com/watch?v=tNrlSai6JGA' 
+url = 'https://www.youtube.com/watch?v=tNrlSai6JGA'
 
-file_tmpl = 'out/' + u.generate_hash(url)
-langs = ["en","ru"]
+def download_youtube_title_and_content(  url
+                                        ,folder_of_files    = 'out/'
+                                        ,sub_ext            = 'vtt'
+                                        ,sub_table_ext      = 'tsv'
+                                        ,langs              = ["en","ru"]
+                                      ):
 
-files = []
-for l in langs:
-    files.append(dict( _in = file_tmpl + '.'+ l + '.vtt' 
-                      ,_out = file_tmpl + '.'+ l + '.tsv'  ))
+
+    directory       = Path(folder_of_files)
+    hash_word       = u.generate_hash(url)
+    files           = list(directory.glob(hash_word+'*'))
 
 
-ydl_opts = {
-    "subtitleslangs": langs,
-    'writeautomaticsub': True,
-    "writesub":  True ,
-    "embedsubs": True,
-    'writedescription': True,
-    'subtitlesformat': 'vtt',
-    'skip_download': True,
+    file_tmpl = folder_of_files + hash_word
+    # langs = ["en","ru"]
 
-    'verbose': True,
+    files = []
+    for l in langs:
+        files.append(dict(    _in  = file_tmpl + '.'+ l + '.' + sub_ext 
+                            ,_out  = file_tmpl + '.'+ l + '.' + sub_table_ext 
+                            ,_lang = l   ))
+
+
+    ydl_opts = {
+        "subtitleslangs": langs,
+        'writeautomaticsub': True,
+        "writesub":  True ,
+        "embedsubs": True,
+        'writedescription': True,
+        'subtitlesformat': sub_ext,
+        'skip_download': True,
+
+        'outtmpl': file_tmpl
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(url)
+        info_dict = ydl.extract_info(url, download=False)
+        
+        video_title = info_dict.get('title', None)
+        # TODO maybe we can avoid extracting to the file by _write_subtitles(self, info_dict, filename) 
+        # and write subs to variable...
+        # https://stackoverflow.com/questions/63916090/extract-the-title-of-a-youtube-video-python
+        su          = info_dict.get('requested_subtitles', None)
+        video_lang  = info_dict.get('language', None)
+
+    # print(video_title)
+    # print(su)
+        
+    files =  sorted(files, key=lambda x: (x['_lang'] == video_lang),reverse=True)
+    print(files)
+        
+    sent =''
+    for f in files:
+        with open(f['_out'],'w',encoding='utf8') as fl:
+            fl.write('start\tend\ttext\n')
+            fl.write(u.fix_youtube_vtt(f['_in']))
+            sent+='# '+ f['_lang'] + '\n'
+            sent += tsv_to_md(f['_out'] ,url )
+
+            
     
-    
-    
-    'outtmpl': file_tmpl
-}
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+    with open('out/test.md','w',encoding='utf8') as fl:
+         fl.write(sent)
 
-# f = open('out/d5cce4b58b67502890cb3286833020da.vtt.en.fixed.vtt','w')
-# f.write(u.fix_youtube_vtt('out/d5cce4b58b67502890cb3286833020da.vtt.en.vtt'))
-# f.close() 
+    return [video_title , sent]
 
-for f in files:
-    with open(f['_out'],'w',encoding='utf8') as fl:
-        fl.write(u.fix_youtube_vtt(f['_in']))
-    if os.path.exists(f['_in']): os.remove(f['_in'])
-
+# download_youtube_title_and_content("https://www.youtube.com/watch?v=Z8yL3zkudZU")
+# download_youtube_title_and_content('https://www.youtube.com/watch?v=P1u3UZQmaRo' )
+download_youtube_title_and_content('https://www.youtube.com/watch?v=tNrlSai6JGA')
